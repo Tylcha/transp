37c857e76c756802609dbe3e003dd85229bbf76b
DALTONLAR\SEZAİ <a>
2024-10-07 16:57:31 +0300

    +add comment line

--- src/auth/roles.decorator.ts
+++ src/auth/roles.decorator.ts
@@ -1,5 +1,8 @@
 import { SetMetadata } from "@nestjs/common";
 import { Role } from "../users/enum/users.role.enum";
 
+// 'roles' adında bir metadata key'i oluşturuyoruz
 export const ROLES_KEY = 'roles';
+
+// ...roles parametresi birden fazla role alabilir. Bu roller metadata olarak eklenir
 export const Roles = (...roles: Role[]) => SetMetadata(ROLES_KEY,roles);
